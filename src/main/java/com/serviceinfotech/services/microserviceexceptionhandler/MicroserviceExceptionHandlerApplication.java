0cd4f3d19667b326625b7c2333cc460be422778b
prashantnaik <prashantnaik@outlook.com>
2018-06-17 15:10:44 +0100

    Intial Commit

---
+++ src/main/java/com/serviceinfotech/services/microserviceexceptionhandler/MicroserviceExceptionHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.serviceinfotech.services.microserviceexceptionhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceExceptionHandlerApplication.class, args);
+	}
+}
